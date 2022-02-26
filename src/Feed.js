8469f4a69ed66ba7c15e79b72cc9e74fed82cd7c
Shubham Vishwakarma <pikachu@pop-os.localdomain>
2022-02-26 12:17:46 +0530

    adding project

---
+++ src/Feed.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Joke from './Joke';
+import "./Feed.css";
+import laughing from "./laughing.png";
+
+class Feed extends Component {
+    static defaultProps = {
+        NewJokesNumber : 5
+    }
+    constructor(props) {
+        super(props);
+        this.state = {
+            jokes: []
+        }
+        this.add_jokes = this.add_jokes.bind(this);
+        this.VoteUp = this.VoteUp.bind(this);
+        this.VoteDown = this.VoteDown.bind(this);
+    }
+
+    async add_jokes() {
+        let new_jokes = [];
+        for (let i=0;i<this.props.NewJokesNumber;i++) {
+            let res = await axios.get("https://icanhazdadjoke.com/", 
+            {headers:{Accept:"application/json"}});
+            new_jokes.push({
+                joke: res.data.joke,
+                id:res.data.id, 
+                UpScore:0,
+                DownScore: 0,
+                score:0,
+                rating: 1
+            });
+        }
+        this.setState(currState => (
+            { jokes: [...currState.jokes, ...new_jokes]}
+        ));
+    }
+
+    async componentDidMount(){
+        this.add_jokes();
+    }
+
+    VoteUp(id) {
+        this.setState(currState => {
+            let modified_jokes = currState.jokes.map(joke => {
+                let modified_joke = joke;
+                if (joke.id === id ) {
+                    joke.UpScore = joke.UpScore + 0.5;
+                    joke.score = joke.score + 0.5;
+                    joke.rating = (joke.UpScore)/(joke.DownScore+1);
+                }
+                return modified_joke;
+            })
+            return {jokes: modified_jokes};
+        })
+    }
+
+    VoteDown(id) {
+        this.setState(currState => {
+            let modified_jokes = currState.jokes.map(joke => {
+                let modified_joke = joke;
+                if (joke.id === id ) {
+                    joke.DownScore = joke.DownScore + 0.5;
+                    joke.score = joke.score - 0.5;
+                    joke.rating = (joke.UpScore)/(joke.DownScore+1);
+                }
+                return modified_joke;
+            })
+            return {jokes: modified_jokes};
+        })
+    }
+
+    render() {
+        return (
+            <div className="Feed">
+                <div className="Feed-Sidebar">
+                    <h2 className='Feed-Sidebar-Title'> <strong>Dad</strong> Jokes</h2>
+                    <img className="Feed-Emote" src={laughing} alt="rofl-emoji"/>
+                    <div>
+                    <button className="Feed-Btn" onClick={this.add_jokes}>Add Jokes </button>
+                    </div>
+                </div>
+                
+                <div className="Feed-Joke-List">
+                        {this.state.jokes.map(joke => (
+                            <Joke 
+                                joke={joke.joke} 
+                                id={joke.id} 
+                                key={joke.id} 
+                                score={joke.score}
+                                rating={joke.rating}
+                                VoteUp = {this.VoteUp}
+                                VoteDown = {this.VoteDown}
+                                UpScore = {joke.UpScore}
+                                DownScore = {joke.DownScore}
+                            />
+                        ))}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Feed;
---
+++ src/Joke.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import "./Joke.css";
+
+class Joke extends Component {
+    constructor(props) {
+        super(props);
+        this.handleVoteUp = this.handleVoteUp.bind(this);
+        this.handleVoteDown = this.handleVoteDown.bind(this);
+    }
+
+    handleVoteUp() {
+        this.props.VoteUp(this.props.id);
+    }
+
+    handleVoteDown() {
+        this.props.VoteDown(this.props.id);
+    }
+
+    render() {
+        let scoreClass="Score";
+
+        let emojis = [
+            <i class="emoji em em-no_mouth" aria-label="FACE WITHOUT MOUTH"></i>,
+            <i class="emoji em em-rolling_on_the_floor_laughing" aria-label="ROLLING ON THE FLOOR LAUGHING"></i>,
+            <i class="emoji em em-laughing" aria-label="SMILING FACE WITH OPEN MOUTH AND TIGHTLY-CLOSED EYES"></i>,
+            <i class="emoji em em-smiley" aria-label="SMILING FACE WITH OPEN MOUTH"></i>,
+            <i class="emoji em em-expressionless" aria-label="EXPRESSIONLESS FACE"></i>,
+            <i class="emoji em em-angry" aria-label="ANGRY FACE"></i>,
+        ];
+
+        let emoji_index = 0;
+        const rating = this.props.rating;
+
+        if(this.props.UpScore!==0 || this.props.DownScore!==0){
+            if (rating>6) {
+                emoji_index = 1;
+                scoreClass = "Score good-Score";
+            }
+            else if (rating > 4) {
+                emoji_index = 2;
+                scoreClass = "Score good-Score"                
+            }
+            else if (rating > 3) {
+                emoji_index = 3;
+                scoreClass = "Score";
+            }
+            else if (rating > 2) {
+                emoji_index = 4;
+                scoreClass = "Score bad-Score";
+            }
+            else  {
+                emoji_index = 5;
+                scoreClass = "Score bad-Score";
+            }
+        }
+        
+        
+
+
+        return (
+            <div className="Joke-Container">
+                <div className='Joke'>
+                    <div className='Score-card'>
+                        <i 
+                            onClick={this.handleVoteDown}
+                            className="vote-Btn fa-solid fa-arrow-down"
+                        />
+                        
+                        <div className={scoreClass}>
+                            {this.props.score}
+                        </div>
+
+                        <i 
+                            onClick={this.handleVoteUp}
+                            className="vote-Btn fa-solid fa-arrow-up"
+                        />
+                    </div>
+                    
+                    <div className="Joke-text">
+                        <p> {this.props.joke} </p>
+                    </div>
+                    <div className="Joke-emoji">
+                        {emojis[emoji_index]}
+                    </div>
+
+                </div>
+                
+            </div>
+        )
+    }
+}
+
+export default Joke;
